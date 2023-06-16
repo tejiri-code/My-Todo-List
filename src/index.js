25b758d93ba8302ebfc99928ade15fecc11826ea
tejiri-code <evelynedjere@gmail.com>
2023-06-16 21:42:56 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import TodoApp from "./TodoApp";
+import Analytics from '@vercel/analytics';
+
+const analytics = Analytics('your-project-id');
 
 ReactDOM.render(
   <React.StrictMode>
